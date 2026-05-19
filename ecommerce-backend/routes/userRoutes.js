3bf6149ddf13735a24b3c9aa4893def60abb2411
vaishnavi <vaishnavi@qburst.com>
2026-05-19 16:19:36 +0530

    removed the user admin apis

--- ecommerce-backend/routes/userRoutes.js
+++ ecommerce-backend/routes/userRoutes.js
@@ -1,14 +1,14 @@
 import express from "express";
-import { authUser, registerUser, getUserProfile, getUsers, getUserById, updateUser, deleteUser, updateUserProfile, logoutUser } from "../controller/userController.js";
+import { authUser, registerUser, logoutUser } from "../controller/userController.js";
 import { protect } from "../middleware/authMiddleware.js";
 const router = express.Router();
 /** Parses Postman "form-data" (multipart/form-data); JSON still uses express.json() */
 
-router.route("/").post(registerUser).get(getUsers);
+router.route("/").post(registerUser);
 router.route("/login").post(authUser);
 router.post("/logout", logoutUser);
 router.get("/logout", logoutUser);
-router.route("/profile").get(protect,getUserProfile).put(protect,updateUserProfile);
-router.route("/:id").get(getUserById).put(updateUser).delete(deleteUser);
+// router.route("/profile").get(protect,getUserProfile).put(protect,updateUserProfile);
+// router.route("/:id").get(getUserById).put(updateUser).delete(deleteUser);
 
 export default router;
