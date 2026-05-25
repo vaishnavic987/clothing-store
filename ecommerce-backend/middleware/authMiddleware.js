74dfb61282d503fd3735d58db7d8ba8b90ba166e
vaishnavi <vaishnavi@qburst.com>
2026-05-25 15:29:55 +0530

    Revert "added token from bearer"
    This reverts commit 589396663057f61e0a0efa745f47a0bb045c3a52.

--- ecommerce-backend/middleware/authMiddleware.js
+++ ecommerce-backend/middleware/authMiddleware.js
@@ -3,12 +3,8 @@ import asyncHandler from "./asyncHandler.js";
 import User from "../models/userModel.js";
 
 export const protect = asyncHandler(async (req, res, next) => {
-    let token = req.cookies.jwt;
-
-    if (!token && req.headers.authorization?.startsWith("Bearer")) {
-        token = req.headers.authorization.split(" ")[1];
-    }
-
+    let token;
+    token = req.cookies.jwt;
     if (token) {
         try {
             const decoded = jwt.verify(token, process.env.JWT_SECRET);
